e314cfa130871e059474edf372b39cf5861c3d13
Nayan <91504165+wizenheimer@users.noreply.github.com>
2023-12-23 17:19:42 +0530

    fix: typos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-					{"I'm building the stripe of contracts at "}
+					{"I'm building the stripe for contracts at "}
 					<Link
 						target="_blank"
 						href="https://cyyrus.com"
