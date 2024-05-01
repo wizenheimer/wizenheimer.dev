3c0021ee9b13221fd088361cdc7f33c549186ce0
Nayan <wizenheimer.dev@gmail.com>
2024-05-01 18:19:08 +0530

    feat: update landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,13 +36,13 @@ export default function Home() {
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-					{"I'm building the stripe for contracts at "}
+					{"I'm currently working on Vector Search and Model Inference at "}
 					<Link
 						target="_blank"
-						href="https://cyyrus.com"
+						href="https://couchbase.com"
 						className="underline duration-500 hover:text-zinc-300"
 					>
-						cyyrus
+						Couchbase
 					</Link>{" "}
 				</h2>
 			</div>
